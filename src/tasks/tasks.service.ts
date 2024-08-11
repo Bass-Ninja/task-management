36c0cfd40ba85206bdc81e4beaeeaafd8f430397
Nina Emeršič <nina.emersic@inova.si>
2024-08-11 23:58:13 +0200

    Add TasksModule

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,61 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'UUID';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] =
+        [];
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { search, status } = filterDto;
+        let tasks: Task[] = this.getAllTasks();
+        if (status) {
+            tasks = tasks.filter((task: Task) => task.status === status);
+        }
+        if (search) {
+            tasks = tasks.filter((task: Task) => {
+                return task.title.toLowerCase().includes(search.toLowerCase()) || task.description.toLowerCase().includes(search.toLowerCase());
+            });
+        }
+        return tasks;
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        }
+        this.tasks.push(task);
+        return task;
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const task: Task = this.getTaskById(id);
+        task.status = status;
+        return task;
+    }
+
+    getTaskById(id: string): Task {
+        const task: Task = this.tasks.find((task: Task) => task.id === id);
+        if (!task) {
+            throw new NotFoundException(`Task with "${id} not found."`);
+        }
+        return  task;
+    }
+
+    deleteTask(id: string): void {
+        const found: Task = this.tasks.find((task: Task) => task.id === id);
+        this.tasks.filter((task: Task) => task.id !== found.id);
+
+    }
+}
